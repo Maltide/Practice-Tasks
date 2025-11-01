7dafa879cdc094241128982e276944a886dce450
Maltide <maltid1998@gmail.com>
2025-11-01 09:49:16 +0300

    basic stack-functions

--- sprints/sprint-05-data-structures/homework/stack/stack.go
+++ sprints/sprint-05-data-structures/homework/stack/stack.go
@@ -1,4 +1,7 @@
+// package problems contains basic stack-functions and tests for them
 package problems
+
+import "fmt"
 
 // Stack represents a stack data structure
 type Stack struct {
@@ -8,36 +11,53 @@ type Stack struct {
 // NewStack creates a new empty stack
 func NewStack() *Stack {
 	// TODO: implement
-	return nil
+	return &Stack{items: []int{}}
 }
 
 // Push adds an item to the top of the stack
 func (s *Stack) Push(item int) {
 	// TODO: implement
+	s.items = append(s.items, item)
 }
 
 // Pop removes and returns the top item from the stack
 // Returns an error if the stack is empty
 func (s *Stack) Pop() (int, error) {
 	// TODO: implement
-	return 0, nil
+	top, err := s.Peek()
+	if err != nil {
+		return 0, err
+	}
+	s.items = s.items[:len(s.items)-1]
+	return top, nil
 }
 
 // Peek returns the top item without removing it
 // Returns an error if the stack is empty
 func (s *Stack) Peek() (int, error) {
 	// TODO: implement
-	return 0, nil
+	if s.IsEmpty() {
+		return 0, fmt.Errorf("empty stack")
+	}
+	top := s.items[len(s.items)-1]
+	return top, nil
 }
 
 // IsEmpty checks if the stack is empty
 func (s *Stack) IsEmpty() bool {
 	// TODO: implement
+	if len(s.items) == 0 {
+		return true
+	}
 	return false
 }
 
 // Size returns the number of items in the stack
 func (s *Stack) Size() int {
 	// TODO: implement
-	return 0
+	count := 0
+	for range s.items {
+		count++
+	}
+	return count
 }
