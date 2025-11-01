f61a07586b97fc484cd0f221e6507fd67ffd8bc0
Maltide <maltid1998@gmail.com>
2025-11-01 11:59:01 +0300

    minstack task done

--- sprints/sprint-05-data-structures/homework/stack/min_stack.go
+++ sprints/sprint-05-data-structures/homework/stack/min_stack.go
@@ -9,43 +9,75 @@ type MinStack struct {
 // NewMinStack creates a new MinStack
 func NewMinStack() *MinStack {
 	// TODO: implement
-	return nil
+	return &MinStack{stack: []int{}, minStack: []int{}}
 }
 
 // Push adds an element to the top of the stack
 func (ms *MinStack) Push(val int) {
 	// TODO: implement
+	ms.stack = append(ms.stack, val)
+	top, flag := ms.Top()
+	if !flag {
+		ms.minStack = append(ms.minStack, val)
+	} else {
+		if val <= top {
+			ms.minStack = append(ms.minStack, val)
+		} else {
+			ms.minStack = append(ms.minStack, top)
+		}
+	}
 }
 
 // Pop removes the element on the top of the stack
 // Returns the popped value and a boolean indicating success
 func (ms *MinStack) Pop() (int, bool) {
 	// TODO: implement
-	return 0, false
+	if ms.IsEmpty() {
+		return 0, false
+	}
+	top := ms.stack[len(ms.stack)-1]
+	ms.stack = ms.stack[:len(ms.stack)-1]
+	ms.minStack = ms.minStack[:len(ms.minStack)-1]
+	return top, true
 }
 
 // Top gets the top element of the stack
 // Returns the top element and a boolean indicating success
 func (ms *MinStack) Top() (int, bool) {
 	// TODO: implement
-	return 0, false
+	if ms.IsEmpty() {
+		return 0, false
+	}
+	top := ms.minStack[len(ms.minStack)-1]
+	return top, true
 }
 
 // GetMin retrieves the minimum element in the stack
 // Returns the minimum element and a boolean indicating success
 func (ms *MinStack) GetMin() (int, bool) {
 	// TODO: implement
-	return 0, false
+	if ms.IsEmpty() {
+		return 0, false
+	}
+	minelem, _ := ms.Top()
+	return minelem, true
 }
 
 // IsEmpty checks if the stack is empty
 func (ms *MinStack) IsEmpty() bool {
 	// TODO: implement
+	if len(ms.stack) == 0 || len(ms.minStack) == 0 {
+		return true
+	}
 	return false
 }
 
 // Size returns the number of items in the stack
 func (ms *MinStack) Size() int {
 	// TODO: implement
-	return 0
+	count := 0
+	for range ms.stack {
+		count++
+	}
+	return count
 }
