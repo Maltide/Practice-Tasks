6dde932e7351dca3f6068076b20465f19e4cb9ac
Maltide <maltid1998@gmail.com>
2025-11-02 11:47:04 +0300

    Queue-basic done

--- sprints/sprint-05-data-structures/homework/queue/queue.go
+++ sprints/sprint-05-data-structures/homework/queue/queue.go
@@ -1,5 +1,7 @@
 package problems
 
+import "fmt"
+
 // Queue represents a basic FIFO (First In, First Out) queue
 type Queue struct {
 	items []int
@@ -14,13 +16,23 @@ func NewQueue() *Queue {
 // Push adds an element to the back of the queue
 func (q *Queue) Push(item int) {
 	// TODO: implement
+	q.items = append(q.items, item)
+	fmt.Printf("Appending item %v. Queue now is %v\n", item, q.items)
 }
 
 // Pull removes and returns the element from the front of the queue
 // Returns the pulled element and a boolean indicating success
 func (q *Queue) Pull() (int, bool) {
 	// TODO: implement
-	return 0, false
+	if q.IsEmpty() {
+		return 0, false
+	}
+	fmt.Println("Pulling:")
+	front := q.items[0]
+	fmt.Printf("front is %v\n", front)
+	q.items = q.items[1:]
+	fmt.Printf("Cut queue. Now it's%v\n", q.items)
+	return front, true
 }
 
 // Front returns the element at the front of the queue without removing it
