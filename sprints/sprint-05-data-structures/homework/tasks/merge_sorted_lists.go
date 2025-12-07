5aeaa73c1c3b145f84271b92e4968570c487db0f
Maltide <maltid1998@gmail.com>
2025-12-08 01:21:56 +0300

    merged-list-task

--- sprints/sprint-05-data-structures/homework/tasks/merge_sorted_lists.go
+++ sprints/sprint-05-data-structures/homework/tasks/merge_sorted_lists.go
@@ -4,12 +4,70 @@ package problems
 // Returns the head of the merged linked list
 func MergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	// TODO: implement
-	return nil
+	if list1 == nil && list2 == nil {
+		return nil
+	}
+
+	if list1 == nil {
+		return list2
+	}
+
+	if list2 == nil {
+		return list1
+	}
+
+	resList := &ListNode{}
+
+	current := resList
+
+	for list1 != nil && list2 != nil {
+		if list1.Val <= list2.Val {
+			current.Next = list1
+
+			list1 = list1.Next
+
+			current = current.Next
+		} else {
+			current.Next = list2
+
+			list2 = list2.Next
+
+			current = current.Next
+		}
+	}
+
+	if list1 != nil {
+		current.Next = list1
+	}
+
+	if list2 != nil {
+		current.Next = list2
+	}
+
+	return resList.Next
 }
 
 // MergeTwoListsRecursive merges two sorted linked lists recursively
 // Returns the head of the merged linked list
 func MergeTwoListsRecursive(list1 *ListNode, list2 *ListNode) *ListNode {
 	// TODO: implement
-	return nil
+	if list1 == nil && list2 == nil {
+		return nil
+	}
+
+	if list1 == nil {
+		return list2
+	}
+
+	if list2 == nil {
+		return list1
+	}
+
+	if list1.Val <= list2.Val {
+		list1.Next = MergeTwoListsRecursive(list1.Next, list2)
+		return list1
+	} else {
+		list2.Next = MergeTwoListsRecursive(list2.Next, list1)
+		return list2
+	}
 }
