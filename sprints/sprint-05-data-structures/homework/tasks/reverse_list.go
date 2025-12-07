9ca5872196a1d4742925b8d0565629ffb820dcec
Maltide <maltid1998@gmail.com>
2025-12-08 02:31:53 +0300

    reverse/resursive list

--- sprints/sprint-05-data-structures/homework/tasks/reverse_list.go
+++ sprints/sprint-05-data-structures/homework/tasks/reverse_list.go
@@ -4,12 +4,46 @@ package problems
 // Returns the new head of the reversed list
 func ReverseList(head *ListNode) *ListNode {
 	// TODO: implement
-	return nil
+	nextNode := (*ListNode)(nil)
+
+	prev := (*ListNode)(nil)
+
+	current := head
+
+	for {
+		if current == nil {
+			break
+		}
+		nextNode = current.Next
+
+		current.Next = prev
+
+		prev = current
+
+		current = nextNode
+	}
+	return prev
 }
 
 // ReverseListRecursive reverses a singly linked list using recursion
 // Returns the new head of the reversed list
 func ReverseListRecursive(head *ListNode) *ListNode {
 	// TODO: implement
-	return nil
+	if head == nil {
+		return nil
+	}
+
+	current := head
+
+	if current.Next == nil {
+		return current
+	}
+
+	newHead := ReverseListRecursive(current.Next)
+
+	current.Next.Next = current
+
+	current.Next = nil
+
+	return newHead
 }
